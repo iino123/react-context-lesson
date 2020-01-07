513d2faa15d27b55551bcf3c9bc32f6e8fc3ff5a
iinokenjiro <zxcvbnm.ken@gmail.com>
2020-01-07 14:53:02 +0900

    update: useHooks on cartItemCount & totalPrice

--- src/providers/cart/cart.provider.js
+++ src/providers/cart/cart.provider.js
@@ -16,7 +16,23 @@ const CartProvider = ({ children }) => {
   const [hidden, setHidden] = useState(true);
   const [cartItems, setCartItems] = useState([]);
   const [cartItemsCount, setCartItemsCount] = useState(0);
+  const [totalPrice, setTotalPrice] = useState(0);
   const toggleHidden = () => setHidden(!hidden);
+
+  useEffect(() => {
+    // TODO: ロジック部分はutilに移動
+    const Count = cartItems.reduce(
+      (acc, cartItem) => acc + cartItem.quantity,
+      0
+    );
+    const totalPrice = cartItems.reduce(
+      (acc, cartItem) => acc + cartItem.quantity * cartItem.price,
+      0
+    );
+    console.log(totalPrice);
+    setCartItemsCount(Count);
+    setTotalPrice(totalPrice);
+  }, [cartItems]);
 
   // addItemToCart(item)の返り値が新たなカート内の状態を返す->それをsetCartItemでローカルステートにセットする
   const addItem = item => setCartItems(addItemToCart(cartItems, item));
@@ -30,7 +46,8 @@ const CartProvider = ({ children }) => {
         cartItems,
         addItem,
         removeItem,
-        cartItemsCount
+        cartItemsCount,
+        totalPrice
       }}
     >
       {children}
